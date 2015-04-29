4195917dc436e466b95007c26e6bf13c0e343167
Abby Meunier <abby.meunier@gmail.com>
2015-04-29 14:27:33 -0700

    generate new grid

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 $(document).ready(function() {
-	var numSquares = 256;
-	//create 16x16 grid
-	for(var i = 0; i < numSquares; i++) {
-		if(i % 16 === 0) {
+	generateGrid(16);
+
+	$("#button").click(function() {
+		//get new grid dimensions
+		var newDimension = prompt("How many squares per row/column?");	
+		
+		if(newDimension != null) {
+			//empty current grid
+			$("#container").empty();
+			//generate new grid
+			generateGrid(newDimension);
+		}
+	});	
+});
+
+var generateGrid = function(numSquares) {
+	var squareSize = 400 / numSquares;
+	
+	for(var i = 0; i < numSquares * numSquares; i++) {
+		if(i % numSquares  === 0) {
 			$("#container").append("<div class='squares' id='newRow'></div>");
 		} else {
 			$("#container").append("<div class='squares'></div>");
 		}
 	}
-	//highlight square if mouse passes over it
+	
+	//adjust squares to fit container
+	$(".squares").css("width", squareSize);
+	$(".squares").css("height", squareSize);
+		
+	highlight();
+};
+
+var highlight = function() {
 	$(".squares").hover(function() {
 		$(this).addClass("highlighted");	
-	});	
-});
+	});
+};
+
