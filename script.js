424d3b9a2e396f58c553796fb4ad1799027076c3
Abby Meunier <abby.meunier@gmail.com>
2015-04-29 13:07:13 -0700

    hover over square to highlight

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var numSquares = 256;
+	//create 16x16 grid
 	for(var i = 0; i < numSquares; i++) {
 		if(i % 16 === 0) {
 			$("#container").append("<div class='squares' id='newRow'></div>");
@@ -7,4 +8,8 @@ $(document).ready(function() {
 			$("#container").append("<div class='squares'></div>");
 		}
 	}
+	//highlight square if mouse passes over it
+	$(".squares").hover(function() {
+		$(this).addClass("highlighted");	
+	});	
 });
